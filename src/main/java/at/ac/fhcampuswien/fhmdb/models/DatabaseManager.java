990c7486722e528ec738fa5218da2e7f29cbcec8
FabiWetz23 <139136925+FabiWetz23@users.noreply.github.com>
2024-05-07 20:45:55 +0200

    fixed

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/DatabaseManager.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/DatabaseManager.java
@@ -1,5 +1,4 @@
 package at.ac.fhcampuswien.fhmdb.models;
-
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
@@ -13,10 +12,11 @@ public class DatabaseManager {
     private static final String username="username";
     private static final String password="pass";
     private static ConnectionSource connectionSource;
+    Dao<MovieEntity, Long> dao;
 
     private static void createConnectionSource() throws SQLException {
         JdbcConnectionSource source = new JdbcConnectionSource(url, username, password);
     }
-    Dao<MovieEntity, String> accountDao;
+
 
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieEntity.java
@@ -1,11 +1,13 @@
+package at.ac.fhcampuswien.fhmdb.models;
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-@DatabaseTable
+@DatabaseTable (tableName = "Movie")
 public class MovieEntity {
-
+    @DatabaseField(generatedId = true)
     private Long id;
-
+    @DatabaseField()
     private String title;
-
+    @DatabaseField()
     private String genres; // Genres als String, durch "," getrennt
 
     // Weitere Eigenschaften und Getter/Setters
