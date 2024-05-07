6fe201600f3cdee16013e929232e1b939fa9afb4
FabiWetz23 <139136925+FabiWetz23@users.noreply.github.com>
2024-05-07 21:42:03 +0200

    Datenbank

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/DatabaseManager.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/DatabaseManager.java
@@ -1,7 +1,9 @@
 package at.ac.fhcampuswien.fhmdb.models;
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,7 +15,30 @@ public class DatabaseManager {
     private static final String password="pass";
     private static ConnectionSource connectionSource;
     Dao<MovieEntity, Long> dao;
+    private static DatabaseManager instance;
+    public void TestDB() throws SQLException {
+        MovieEntity movie = new MovieEntity("title","description",12,"img",12,"director","writer","Cast",12);
+        dao.create(movie);
+    }
 
+    private DatabaseManager(){
+        try {
+            createConnectionSource();
+            dao= DaoManager.createDao(connectionSource, MovieEntity.class);
+            createTables();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        }
+        private static void createTables() throws SQLException {
+            TableUtils.createTableIfNotExists(connectionSource, MovieEntity.class);
+        }
+        public static DatabaseManager getDatabaseManager(){
+            if(instance==null){
+                instance = new DatabaseManager();
+        }
+            return instance;
+    }
     private static void createConnectionSource() throws SQLException {
         JdbcConnectionSource source = new JdbcConnectionSource(url, username, password);
     }
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieEntity.java
@@ -3,12 +3,53 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable (tableName = "Movie")
 public class MovieEntity {
-    @DatabaseField(generatedId = true)
+    @DatabaseField(id = true,generatedId = true)
     private Long id;
-    @DatabaseField()
+
+
+    @DatabaseField
     private String title;
-    @DatabaseField()
-    private String genres; // Genres als String, durch "," getrennt
+
+    @DatabaseField
+    private String description;
+
+    /*
+    @DatabaseField
+    private String genres;
+*/
+    @DatabaseField
+    private int releaseYear;
+
+    @DatabaseField
+    private String imgUrl;
+
+    @DatabaseField
+    private int lengthInMinutes;
+
+    @DatabaseField
+    private String directors;
+
+    @DatabaseField
+    private String writers;
+
+    @DatabaseField
+    private String mainCast;
+
+    @DatabaseField
+    private double rating;
 
     // Weitere Eigenschaften und Getter/Setters
+
+
+    public MovieEntity(String title, String description, int releaseYear, String imgUrl, int lengthInMinutes, String directors, String writers, String mainCast, double rating) {
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.imgUrl = imgUrl;
+        this.lengthInMinutes = lengthInMinutes;
+        this.directors = directors;
+        this.writers = writers;
+        this.mainCast = mainCast;
+        this.rating = rating;
+    }
 }
